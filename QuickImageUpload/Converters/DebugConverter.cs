88479cab47352fe240ec3b32cf99d6105c843988
jiewmeng <jiewmeng@gmail.com>
2010-11-30 09:05:34 +0800

    Considered done, focusing on web apps next

---
+++ QuickImageUpload/Converters/DebugConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace QuickImageUpload.Converters
+{
+    class DebugConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            object tmp = value;
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ QuickImageUpload/Converters/StatusToBrushConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using WorkQueueLib;
+using System.Windows.Media;
+
+namespace QuickImageUpload.Converters
+{
+    class StatusToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var status = (WorkStatus)value;
+            SolidColorBrush brush = Brushes.Green;
+            switch (status)
+            {
+                case WorkStatus.Cancelled:
+                    brush = Brushes.DarkGray;
+                    break;
+                case WorkStatus.Error:
+                    brush = Brushes.Red;
+                    break;
+            }
+            return brush;   
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
